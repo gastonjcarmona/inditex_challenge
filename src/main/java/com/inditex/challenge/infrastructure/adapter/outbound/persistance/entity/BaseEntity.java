be6d356475603343a0e73cceff58337dc3f86fe4
Gaston Carmona <gcarmona@Gastons-MacBook-Pro.local>
2021-11-06 12:39:15 -0300

    feat: add entity

---
+++ src/main/java/com/inditex/challenge/infrastructure/adapter/outbound/persistance/entity/BaseEntity.java
@@ -0,0 +1,31 @@
+package com.inditex.challenge.infrastructure.adapter.outbound.persistance.entity;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@EqualsAndHashCode
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, updatable = false)
+    @CreatedDate
+    private LocalDateTime created;
+
+    @Column
+    @LastModifiedDate
+    private LocalDateTime updated;
+
+
+}
---
+++ src/main/java/com/inditex/challenge/infrastructure/adapter/outbound/persistance/entity/Price.java
@@ -0,0 +1,41 @@
+package com.inditex.challenge.infrastructure.adapter.outbound.persistance.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "prices")
+public class Price extends BaseEntity {
+
+    @Column(name = "brand_id", nullable = false)
+    private Long brandId;
+
+    @Column(name = "start_date", nullable = false)
+    private LocalDateTime startDate;
+
+    @Column(name = "end_date", nullable = false)
+    private LocalDateTime endDate;
+
+    @Column(name = "price_list", nullable = false)
+    private Integer priceList;
+
+    @Column(name = "product_id", nullable = false)
+    private Long productId;
+
+    @Column(name = "priority", nullable = false)
+    private Integer priority;
+
+    @Column(name = "price", nullable = false)
+    private Double price;
+
+    @Column(name = "currency", nullable = false)
+    private String currency;
+
+}
